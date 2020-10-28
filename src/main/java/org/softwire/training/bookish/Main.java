c7e1dc75bba45e57ba36985514a50d8e59234500
Hayes <Joshua.Hayes@bp.com>
2020-10-28 12:59:54 +0000

    add book, delete book, view accounts

--- src/main/java/org/softwire/training/bookish/Main.java
+++ src/main/java/org/softwire/training/bookish/Main.java
@@ -1,24 +1,51 @@
 package org.softwire.training.bookish;
 
+import com.mysql.jdbc.JDBC4ClientInfoProvider;
 import org.jdbi.v3.core.Jdbi;
+import org.softwire.training.bookish.models.database.Book;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
         String hostname = "localhost";
-        String database = "bookish";
-        String user = "bookish";
-        String password = "bookish";
+        String database = "database";
+        String user = "root";
+        String password = "admin";
         String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
+        Jdbi jdbi = Jdbi.create(connectionString);
+//        jdbcMethod(connectionString);
+//        jdbiMethod(connectionString);
 
-        jdbcMethod(connectionString);
-        jdbiMethod(connectionString);
+        System.out.println("Hi Librarian, what would you like to do today?  Add book (1) Delete book (2) View accounts (3): ");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        Book bookHandling = new Book();
+        if (input.equals("1")){
+            bookHandling.addBook(sc,jdbi);
+        }else if (input.equals("2")){
+            bookHandling.deleteBook(sc,jdbi);
+        }else if (input.equals("3")){
+            printAccounts(sc,jdbi);
+        }
+
     }
+
+    private static void printAccounts(Scanner sc,Jdbi jdbi){
+        List<Map<String,Object>> allAccounts = jdbi.withHandle(handle -> handle.createQuery("select * from accounts")
+                    .mapToMap()
+                    .list());
+        for (int i = 0; i <allAccounts.size();i++){
+            System.out.println(allAccounts.get(i));
+        }
+    }
+
 
     private static void jdbcMethod(String connectionString) throws SQLException {
         System.out.println("JDBC method...");
@@ -26,9 +53,18 @@ public class Main {
         // TODO: print out the details of all the books (using JDBC)
         // See this page for details: https://docs.oracle.com/javase/tutorial/jdbc/basics/processingsqlstatements.html
 
-        Connection connection = DriverManager.getConnection(connectionString);
+        Connection con = DriverManager.getConnection(connectionString);
 
-
+        String query = "select * from accounts";
+        try (Statement stmt = con.createStatement()) {
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                String accountName = rs.getString("accountName");
+                System.out.print(accountName);
+            }
+        } catch (SQLException e) {
+            System.out.print(e);
+        }
 
     }
 
@@ -40,6 +76,15 @@ public class Main {
         // Use the "Book" class that we've created for you (in the models.database folder)
 
         Jdbi jdbi = Jdbi.create(connectionString);
+        List<String> names = jdbi.withHandle(handle -> {
+                    handle.createQuery("select accountName from accounts")
+                            .mapTo(String.class)
+                            .list();
+                    handle.execute("INSERT INTO accounts(accountName,age) VALUES (?, ?)", "mick", 9);
+                    return null;
+        });
+
+        //System.out.print(Arrays.toString(names.toArray()));
 
 
 
--- src/main/java/org/softwire/training/bookish/models/database/Book.java
+++ src/main/java/org/softwire/training/bookish/models/database/Book.java
@@ -1,5 +1,24 @@
 package org.softwire.training.bookish.models.database;
 
+import org.jdbi.v3.core.Jdbi;
+
+import java.util.Scanner;
+
 public class Book {
+    public void deleteBook(Scanner sc, Jdbi jdbi){
+        System.out.println("What is the name of the book?: ");
+        String bookName = sc.nextLine();
+        System.out.println("And what is the books author?; ");
+        String author = sc.nextLine();
+        jdbi.withHandle(handle -> handle.execute("DELETE FROM books WHERE(bookName = ? && bookAuthor = ?)",bookName,author));
+    }
+
+    public void addBook(Scanner sc,Jdbi jdbi){
+        System.out.println("What is the name of the book?: ");
+        String bookName = sc.nextLine();
+        System.out.println("And what is the books author?; ");
+        String author = sc.nextLine();
+        jdbi.withHandle(handle -> handle.execute("INSERT INTO books(bookName,bookAuthor) VALUES (?,?)",bookName,author));
+    }
 
 }
